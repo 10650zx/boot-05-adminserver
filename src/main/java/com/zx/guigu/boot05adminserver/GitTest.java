4fc1c94ff2526de563499ba5befee30f9996738d
zx3 <106503@qq.com>
2022-09-02 19:48:43 +0800

    second commit

---
+++ src/main/java/com/zx/guigu/boot05adminserver/GitTest.java
@@ -0,0 +1,8 @@
+package com.zx.guigu.boot05adminserver;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git 2");
+    }
+}
---
+++ src/main/java/com/zx/guigu/boot05adminserver/controller/HELLOcONTROLLER.java
@@ -0,0 +1,9 @@
+package com.zx.guigu.boot05adminserver.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestController
+public class HELLOcONTROLLER {
+
+}
